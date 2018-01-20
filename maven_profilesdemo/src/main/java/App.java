d03792ab88e485f5cccac62b5a1447e763c45af5
vvsrk <vvsrk.siva@gmail.com>
2018-01-20 16:06:10 -0500

    commit

---
+++ maven_profilesdemo/src/main/java/App.java
@@ -0,0 +1,19 @@
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+	private static Logger LOGGER = Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        LOGGER.info("Hello World!");
+    }
+    
+}
